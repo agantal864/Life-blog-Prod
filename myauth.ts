a2966107f2190c405bf5cea1c151d7ff63094074
agantal864 <aamagantal@gmail.com>
2025-10-21 01:49:21 +0800

    added secret env

--- myauth.ts
+++ myauth.ts
@@ -5,6 +5,7 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google],
+  secret: process.env.NEXT_PUBLIC_SECRET,
   session: {
     strategy: "jwt",
   },
