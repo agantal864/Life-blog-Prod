248db88a132b76a6a7d9ba9fd4a7ab325dc717a8
agantal864 <aamagantal@gmail.com>
2025-10-21 01:54:55 +0800

    added callback

--- myauth.ts
+++ myauth.ts
@@ -11,18 +11,32 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   },
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    async jwt({ token, user }) {
+     async jwt({ token, user }) {
       if (user) {
         token.sub = user.id;
+        try {
+          const dbUser = await prisma.user.findUnique({
+            where: { id: user.id },
+            select: {
+              isAdmin: true,
+              isMasterAdmin: true,
+            },
+          });
+          token.isAdmin = dbUser?.isAdmin ?? false;
+          token.isMasterAdmin = dbUser?.isMasterAdmin ?? false;
+        } catch (error) {
+          console.error("Error fetching user from DB in JWT callback:", error);
+        }
+        token.isAdmin = (user as any).isAdmin ?? false;
+        token.isMasterAdmin = (user as any).isMasterAdmin ?? false;
       }
       return token;
-    },
-   
+  },
     async session({ session, token }) {
       if (session.user) {
         session.user.id = token.sub as string;
-        // session.user.isAdmin = token.isAdmin as boolean;
-        // session.user.isMasterAdmin = token.isMasterAdmin as boolean;
+        session.user.isAdmin = token.isAdmin as boolean;
+        session.user.isMasterAdmin = token.isMasterAdmin as boolean;
       }
       return session;
     },
@@ -31,24 +45,3 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
 
 
-//  async jwt({ token, user }) {
-//       if (user) {
-//         token.sub = user.id;
-//         // try {
-//         //   const dbUser = await prisma.user.findUnique({
-//         //     where: { id: user.id },
-//         //     select: {
-//         //       isAdmin: true,
-//         //       isMasterAdmin: true,
-//         //     },
-//         //   });
-//         //   token.isAdmin = dbUser?.isAdmin ?? false;
-//         //   token.isMasterAdmin = dbUser?.isMasterAdmin ?? false;
-//         // } catch (error) {
-//         //   console.error("Error fetching user from DB in JWT callback:", error);
-//         // }
-//         token.isAdmin = (user as any).isAdmin ?? false;
-//         token.isMasterAdmin = (user as any).isMasterAdmin ?? false;
-//       }
-//       return token;
-//     },
