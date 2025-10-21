5d861a9adeed4fca8f3c93be030d0bfcde19bd57
agantal864 <aamagantal@gmail.com>
2025-10-21 15:12:13 +0800

    123

--- lib/logrocket-init.ts
+++ lib/logrocket-init.ts
@@ -1,14 +1,14 @@
-'use client';
+// 'use client';
 
-import { useEffect } from 'react';
-import LogRocket from 'logrocket';
+// import { useEffect } from 'react';
+// import LogRocket from 'logrocket';
 
-export default function LogRocketInit() {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      LogRocket.init('e72gtt/life-blog'); 
-    }
-  }, []);
+// export default function LogRocketInit() {
+//   useEffect(() => {
+//     if (typeof window !== 'undefined') {
+//       LogRocket.init('e72gtt/life-blog'); 
+//     }
+//   }, []);
 
-  return null;
-}
+//   return null;
+// }
