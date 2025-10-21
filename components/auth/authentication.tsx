5cdf2468de0a80f00ce8fc5bfe5842821b218872
agantal864 <aamagantal@gmail.com>
2025-10-21 10:46:15 +0800

    db

--- components/auth/authentication.tsx
+++ components/auth/authentication.tsx
@@ -1,12 +1,10 @@
 'use server';
 import { signIn, signOut} from "@/myauth";
 
-// export const login = async (redirectPath: string) => {
-//     await signIn("google", { redirectTo: redirectPath });
-// }
-export const login = async () => {
-  await signIn("google", { redirectTo: "/" });
+export const login = async (redirectPath: string) => {
+    await signIn("google", { redirectTo: redirectPath });
 }
+
 export const logout = async (redirectPath: string) => {
     await signOut({ redirectTo: redirectPath });
 }
