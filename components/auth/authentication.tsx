7dd6e974c47e972dbc8af18333d26868d9f9413a
agantal864 <aamagantal@gmail.com>
2025-10-21 09:04:33 +0800

    change redirect

--- components/auth/authentication.tsx
+++ components/auth/authentication.tsx
@@ -1,8 +1,11 @@
 'use server';
 import { signIn, signOut} from "@/myauth";
 
-export const login = async (redirectPath: string) => {
-    await signIn("google", { redirectTo: redirectPath });
+// export const login = async (redirectPath: string) => {
+//     await signIn("google", { redirectTo: redirectPath });
+// }
+export const login = async () => {
+  await signIn("google", { redirectTo: "/" });
 }
 export const logout = async (redirectPath: string) => {
     await signOut({ redirectTo: redirectPath });
