267108e0887fc1c27125c0c5a3faad50d83de295
agantal864 <aamagantal@gmail.com>
2025-10-21 09:43:55 +0800

    reworked auth.ts

---
+++ lib/auth.config.ts
@@ -0,0 +1,4 @@
+import Google from "next-auth/providers/github"
+import type { NextAuthConfig } from "next-auth"
+ 
+export default { providers: [Google] } satisfies NextAuthConfig
--- myauth.ts
+++ myauth.ts
@@ -1,42 +1,37 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prismaclient";
-import Google from "next-auth/providers/google";
+import authConfig from "@/lib/auth.config";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Google],
-  secret: process.env.AUTH_SECRET,
-  session: {
-    strategy: "jwt",
-  },
+export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  session: {strategy: "jwt"},
+  ...authConfig,
   callbacks: {
      async jwt({ token, user }) {
       if (user) {
         token.sub = user.id;
-        // try {
-        //   const dbUser = await prisma.user.findUnique({
-        //     where: { id: user.id },
-        //     select: {
-        //       isAdmin: true,
-        //       isMasterAdmin: true,
-        //     },
-        //   });
-        //   token.isAdmin = dbUser?.isAdmin ?? false;
-        //   token.isMasterAdmin = dbUser?.isMasterAdmin ?? false;
-        // } catch (error) {
-        //   console.error("Error fetching user from DB in JWT callback:", error);
-        // }
-        // token.isAdmin = (user as any).isAdmin ?? false;
-        // token.isMasterAdmin = (user as any).isMasterAdmin ?? false;
+        try {
+          const dbUser = await prisma.user.findUnique({
+            where: { id: user.id },
+            select: {
+              isAdmin: true,
+              isMasterAdmin: true,
+            },
+          });
+          token.isAdmin = dbUser?.isAdmin ?? false;
+          token.isMasterAdmin = dbUser?.isMasterAdmin ?? false;
+        } catch (error) {
+          console.error("Error fetching user from DB in JWT callback:", error);
+        }
       }
       return token;
   },
     async session({ session, token }) {
       if (session.user) {
         session.user.id = token.sub as string;
-        // session.user.isAdmin = token.isAdmin as boolean;
-        // session.user.isMasterAdmin = token.isMasterAdmin as boolean;
+        session.user.isAdmin = token.isAdmin as boolean;
+        session.user.isMasterAdmin = token.isMasterAdmin as boolean;
       }
       return session;
     },
